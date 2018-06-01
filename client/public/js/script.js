1a952d4ff98d0e37a1403cf43ffa5023eaa54896
jrizza88 <jetorres88@gmail.com>
2018-05-31 22:55:14 -0400

    completing preliminary site mapping for html and css docs

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,1 +1,15 @@
 console.log("successfully connected!");
+
+$(document).ready(function(){
+  console.log("jQuery loaded!")
+
+
+
+  $("#astroHome").on("click", function(){
+    document.location.href="/";
+    console.log("home button clicked");
+  });
+
+
+
+});  // closing out document.ready function
