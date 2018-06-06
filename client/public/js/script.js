341f0d0b91772c9bdf4c50a13fe5f2b2811bcceb
jrizza88 <jetorres88@gmail.com>
2018-06-06 00:35:40 -0400

    added opacity to hovering image

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -4,12 +4,25 @@ $(document).ready(function(){
   console.log("jQuery loaded!")
 
 
-
   $("#astroHome").on("click", function(){
     document.location.href="/";
     console.log("home button clicked");
   });
 
+  $(".astroMain").click(function(){
+    window.location = $(this).find("a").attr("href")
+  });
 
+  // $(".astroMain").on("mouseenter mouseleave", function(){
+  //   $(this).find('.pics').fadeToggle(100);
+  // });
+
+// toggle opacity when hovering image
+$(".pics").hover(function(){
+       $(this).animate({'opacity': '0.4'}, 100);
+  },
+  function(){
+    $(this).animate({'opacity': '1'}, 100);
+  });
 
 });  // closing out document.ready function
